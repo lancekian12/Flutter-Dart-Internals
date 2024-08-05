bcc436de451a11ca287b0f9ea90f8371aa3a9c90
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-08-06 05:21:48 +0800

    Created demo button and increase the performance

--- lib/ui_updates_demo.dart
+++ lib/ui_updates_demo.dart
@@ -1,23 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_internals/demo_button.dart';
 
-class UIUpdatesDemo extends StatefulWidget {
+class UIUpdatesDemo extends StatelessWidget {
   const UIUpdatesDemo({super.key});
-
-  @override
-  StatefulElement createElement() {
-    print('UIUpdatesDemo CREATEELEMENT called');
-    return super.createElement();
-  }
-
-  @override
-  State<UIUpdatesDemo> createState() {
-    return _UIUpdatesDemo();
-  }
-}
-
-class _UIUpdatesDemo extends State<UIUpdatesDemo> {
-  var _isUnderstood = false;
-
   @override
   Widget build(BuildContext context) {
     print('UIUpdatesDemo BUILD called');
@@ -39,28 +24,7 @@ class _UIUpdatesDemo extends State<UIUpdatesDemo> {
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 24),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      _isUnderstood = false;
-                    });
-                  },
-                  child: const Text('No'),
-                ),
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      _isUnderstood = true;
-                    });
-                  },
-                  child: const Text('Yes'),
-                ),
-              ],
-            ),
-            if (_isUnderstood) const Text('Awesome!'),
+            DemoButton(),
           ],
         ),
       ),
